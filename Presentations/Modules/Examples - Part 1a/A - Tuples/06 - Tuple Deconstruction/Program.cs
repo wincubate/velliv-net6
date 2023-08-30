2ee872fd78b0dff70c3775d9be89a68c39a44a57
Jesper Gulmann Henriksen <jgh@wincubate.net>
2023-08-30 22:21:03 +0200

    Added examples

---
+++ Presentations/Modules/Examples - Part 1a/A - Tuples/06 - Tuple Deconstruction/Program.cs
@@ -0,0 +1,17 @@
+﻿using Wincubate.CS7.Data;
+using static System.Console;
+
+namespace Wincubate.CS7.Slide06
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Employee elJefe = new Employee { FirstName = "Ash", LastName = "Williams" };
+
+            //var (first, last) = elJefe;
+
+            //WriteLine(first);
+        }
+    }
+}
